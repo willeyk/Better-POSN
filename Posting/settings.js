396d4e1a141ac3090d1f832ac8414f556fbdeeaf
Wanishaholmes <36937437+Wanishaholmes@users.noreply.github.com>
2018-04-09 07:17:09 -0700

    Create settings.js- not complete
    handles the functionality of settings page

---
+++ Posting/settings.js
@@ -0,0 +1,16 @@
+var saveToFile = '{"firstname":"wanisha","lastname":"holmes","birthday":"05/01/1992","email":"abc@gmail.com","phone":"555-555-555"}';
+
+
+$(document).ready(function(){
+	
+	obj = JSON.parse(saveToFile);
+	console.log(obj.firstname);
+	
+	//$("#add_listing_info").find(".firstname").html("hello");
+	//templateCardPost.find(".time").html(date);
+	
+	
+	
+	//create edit button 
+	
+});
