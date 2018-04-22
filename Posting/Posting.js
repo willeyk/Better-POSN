e41dbced6a99f25de4a9fe8a0878be7bf7fc6440
Wanishaholmes <36937437+Wanishaholmes@users.noreply.github.com>
2018-04-22 11:06:54 -0700

    Update Posting.js

--- Posting/Posting.js
+++ Posting/Posting.js
@@ -266,63 +266,67 @@ function createPost(obj,i,templateCardPost,user_posts)
 
 --------------------------------------------------*/ 
 
-function handleClientLoad() {
-    // Loads required libraries for functionality
-    gapi.load('client:auth2', initClient);
-}
+ function handleClientLoad() 
+	  {
+        // Loads required libraries for functionality
+        gapi.load('client:auth2', initClient);
+      }
 
-function initClient() 
-{
-	// Initialize the client with API key and Client ID. Scope is set to full access
-	// to Google Drive
-	gapi.client.init(
-	{
-		apiKey: 'AIzaSyBFL7e4jh939anq2JsOUeFCTTP41Jz1GoY',
-		discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
-		clientId: '834692356537-5fn43u118ogta150l995623a7auu52fg.apps.googleusercontent.com',
-		scope: 'https://www.googleapis.com/auth/drive'
-	}).then(function () 
-	{
-		// Listen for sign-in state changes
-		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      function initClient() 
+	  {
+			// Initialize the client with API key and Client ID. Scope is set to full access
+			// to Google Drive
+			gapi.client.init(
+			{
+				apiKey: 'AIzaSyBFL7e4jh939anq2JsOUeFCTTP41Jz1GoY',
+				discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+				clientId: '834692356537-5fn43u118ogta150l995623a7auu52fg.apps.googleusercontent.com',
+				scope: 'https://www.googleapis.com/auth/drive'
+			}).then(function () 
+			{
+				// Listen for sign-in state changes
+				gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-		// Handle the initial sign-in state
-		updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-	});
-}
-// Function called when sign in changes
-function updateSigninStatus(isSignedIn) 
-{
-	// If status changes to signed in, makes call to Google API
-	if (isSignedIn) 
-	{
-		//If POSN is not setup then setup
-		//If POSN is setup then get photo links
-		if( isPOSNSetup() == true)
-		{
-			getPhotoLinks();
-		}
-	}
-}
+				// Handle the initial sign-in state
+				updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+			});
+      }
 
-function handleSignInClick(event) 
-{
-	gapi.auth2.getAuthInstance().signIn();
+	  // Function called when sign in changes
+      function updateSigninStatus(isSignedIn) 
+	  {
+			// If status changes to signed in, makes call to Google API
+			if (isSignedIn) 
+			{
+				//If POSN is not setup then setup
+				if( isPOSNSetup() == true)
+				{
+					//Put here whatever you want to test
+					AddPostToWall();
+				}
+			}
+      }
+
+      function handleSignInClick(event) 
+	  {
+			gapi.auth2.getAuthInstance().signIn();
+      }
+
+      function handleSignOutClick(event) 
+	  {
+			gapi.auth2.getAuthInstance().signOut();
+      }
 	
-}
-function handleSignOutClick(event) 
-{
-	gapi.auth2.getAuthInstance().signOut();
-}
-	 
+	
+	//Function will essentially search for the folder that contains
+	//application data to see if POSN is setup
 	 function isPOSNSetup()
 	 {
 		var folderName = "name= 'POSN_Directory'";
 		var isTrashed = "trashed = false"
 		var query = folderName + 'and' + isTrashed;
 		
-		//Function will essentially search for the folder that contains
-		//application data to see if POSN is setup
+
         gapi.client.drive.files.list(
 		{    
 			 'q' : query
@@ -349,7 +353,7 @@ function handleSignOutClick(event)
 	 }
 	 
 
-	 //Makes folders needed to run the POSN application
+	//Makes folders needed to run the POSN application
 	function makeApplicationFolders()
 	{
 		subFolderNames = ['Photos','Comments','Music','Videos','Other_Files']
@@ -367,6 +371,7 @@ function handleSignOutClick(event)
 		{
 			console.log(response.result);
 			makeSubFolders(response.result.id, subFolderNames);
+			updateWallJSON(response.result.id, '');
 			
         }, function(reason) 
 		{
@@ -377,9 +382,13 @@ function handleSignOutClick(event)
 	//Makes each individual subfolder as specificed
 	function makeSubFolders( directoryID, subFolderNames )
 	{
-		numOfSubFolders = 5;
-		directory = [directoryID]
-		mimeType = 'application/vnd.google-apps.folder'
+		var i = 0;
+		var mimeType = 'application/vnd.google-apps.folder';
+		var callList = [];
+		var httpBatch = gapi.client.newBatch();
+		var numOfSubFolders = 5;
+		var directory = [directoryID];
+
 		for( i = 0; i < numOfSubFolders; i++ )
 		{
 			bodyMetadata = 
@@ -389,20 +398,41 @@ function handleSignOutClick(event)
 				'parents' : directory
 			}
 			
-			gapi.client.drive.files.create(
+			callList.push(gapi.client.drive.files.create(
 			{    
 				resource : bodyMetadata
-			}).then(function(response) 
-			{
-				console.log(response.result);
-				
-			}, function(reason) 
-			{
-				console.log('Error: ' + reason.result.error.message);
-			});
+			}));
+			httpBatch.add(callList[i])
 		}
+		console.log(httpBatch);
+		httpBatch.then(function(onFulfilled)
+		{
+			i = 0;
+			myResult = onFulfilled.result;
+			parseRes = JSON.stringify(onFulfilled.result);
+			fail = parseRes.search('403');
+			if(fail == -1)
+			{
+				for( i = 0; i < numOfSubFolders; i++ )
+				{
+					callList[i].then(function(response)
+					{
+						console.log(response.result);
+
+					}, function(reason)
+					{
+						console.log('Error: ' + reason.result.error.message);
+					});
+				}
+			}
+			else
+			{
+				console.log(filesList);
+				apiBackoff( makeSubFolders, directoryID, subFolderNames)
+			}
+		})
 	}
-	
+		
 	//Shows information about a file's permissions
 	function showPermissions( fileID )
 	{
@@ -441,104 +471,243 @@ function handleSignOutClick(event)
 		});
 	}    
 	
-//Can upload a text file to Google Drive root
-function uploadFile() 
-{
-	//Read in file to be uploaded from upload button
-	var uploadFile = document.getElementById("myFile").files[0];
-	var fileContent; 
-	if (uploadFile) 
+	//Can upload a text file to Google Drive root
+	function uploadFileFromButton() 
 	{
-		var name = uploadFile.name;
-		var fileSize = uploadFile.size;
-		var mimeType = uploadFile.type;
-		var reader = new FileReader();
-		reader.readAsBinaryString(uploadFile);
-		reader.onload = function (evt) 
+		//Read in file to be uploaded from upload button
+		var uploadFile = document.getElementById("myFile").files[0];
+		var fileContent; 
+		if (uploadFile) 
 		{
-			document.getElementById("myFile").innerHTML = evt.target.result;
+			var name = uploadFile.name;
+			var fileSize = uploadFile.size;
+			var mimeType = uploadFile.type;
+			var reader = new FileReader();
+			reader.readAsBinaryString(uploadFile);
+			reader.onload = function (evt) 
+			{
+				document.getElementById("myFile").innerHTML = evt.target.result;
 					
 				//evt.target.result is actual text/data in file
 				//Encoded in base64
-			fileContent = btoa(evt.target.result);	
+				fileContent = btoa(evt.target.result);	
 					
-			//Rest of function below handles uploading to Google Drive
-			var auth_token = gapi.client.getToken().access_token;
-			const boundary = '-------314159265358979323846';
-			const delimiter = "\r\n--" + boundary + "\r\n";
-			const close_delim = "\r\n--" + boundary + "--";
-			var metadata = 
-			{ 
-				"name" : name,
-				"mimeType": mimeType
-			};  
+				//Rest of function below handles uploading to Google Drive
+				var auth_token = gapi.client.getToken().access_token;
+				const boundary = '-------314159265358979323846';
+				const delimiter = "\r\n--" + boundary + "\r\n";
+				const close_delim = "\r\n--" + boundary + "--";
+				var metadata = 
+				{ 
+					 "name" : name,
+					 "mimeType": mimeType
+				};  
 
-			var multipartRequestBody =
-			delimiter +  'Content-Type: application/json\r\n\r\n' +
-			JSON.stringify(metadata) +
-			delimiter +
-			'Content-Type: application/json' + '\n' +
-			'Content-Transfer-Encoding: base64\r\n\r\n' +
-			fileContent +
-			close_delim;
+				var multipartRequestBody =
+				delimiter +  'Content-Type: application/json\r\n\r\n' +
+				JSON.stringify(metadata) +
+				delimiter +
+				'Content-Type: application/json' + '\n' +
+				'Content-Transfer-Encoding: base64\r\n\r\n' +
+				fileContent +
+				close_delim;
 
-			gapi.client.request({ 
-				'path': '/upload/drive/v3/files/',
-				'method': 'POST',
-				'params': {'uploadType': 'multipart'},
-				'headers': { 'Content-Type': 'multipart/form-data; boundary="' + boundary + '"', 'Authorization': 'Bearer ' + auth_token, },
-				'body': multipartRequestBody 
-			}).execute(function(file) { 
-				console.log("Wrote to file " + file.name + " id: " + file.id); 
-			},function(error){
-				console.log(error);
-			});  
+				gapi.client.request({ 
+					'path': '/upload/drive/v3/files/',
+					'method': 'POST',
+					'params': {'uploadType': 'multipart'},
+					'headers': { 'Content-Type': 'multipart/form-data; boundary="' + boundary + '"', 'Authorization': 'Bearer ' + auth_token, },
+					'body': multipartRequestBody 
+				}).execute(function(file) { 
+					console.log("Wrote to file " + file.name + " id: " + file.id); 
+				}, function(error){
+						console.log(error);
+				});  
+			}
+			reader.onerror = function (evt) {
+				document.getElementById("myFile").innerHTML = "error reading file";
+			}
 		}
-		reader.onerror = function (evt) {
-			document.getElementById("myFile").innerHTML = "error reading file";
-		}
-	}
-}
-	  
-	  //gives a list of web links for images in the Photos folder
-function getPhotoLinks()
-{
-	var order = 'folder'
-	var photoDirectory = "'1VsFK73KutpZziLFYIihvXlhPIHpH8zbd' in parents"
-	var isTrashed = "trashed = false"
-	//Function returns a list of files in google drive
-    var listPromise = Promise.resolve(gapi.client.drive.files.list(
-	{    
-		'orderBy': order,
-		'q' : photoDirectory
-	}).then(function(response) 
-	{
-		console.log(response.result.files);
-		var filesList = response.result.files;
-		var webLinkList = [];
-		getWebLinks( filesList, webLinkList );
-		return webLinkList;
-	}, function(reason) 
-	{
-		console.log('Error: ' + reason.result.error.message);
-	}));
-			
-	listPromise.then(function(value)
-	{
-		//Do whatever you need with list of Weblinks
-		//value is webLinkList
-		console.log(value);
-		return value;
-	}).then(function(value)
-	{
-		//Do whatever you need with list of Weblinks
-		//value is webLinkList
-	});
-}
+	 }
 	 
-	  
+	// Function posts updated Wall JSON to Google Drive
+	function updateWallJSON( directoryID, fileContent )
+	{
+		var directory = [directoryID];
+		var name = 'myWallJSON.txt';
+		var mimeType = 'text/plain';
+		var baseFileContent = btoa(fileContent);
+		var auth_token = gapi.client.getToken().access_token;
+		const boundary = '-------314159265358979323846';
+		const delimiter = "\r\n--" + boundary + "\r\n";
+		const close_delim = "\r\n--" + boundary + "--";
+		var metadata = 
+		{ 
+			"name" : name,
+			"mimeType": mimeType,
+			"parents" : directory
+		};  
+		var multipartRequestBody =
+		delimiter +  'Content-Type: application/json\r\n\r\n' +
+		JSON.stringify(metadata) +
+		delimiter +
+		'Content-Type: application/json' + '\n' +
+		'Content-Transfer-Encoding: base64\r\n\r\n' +
+		baseFileContent +
+		close_delim;
+
+		gapi.client.request(
+		{ 
+			'path': '/upload/drive/v3/files/',
+			'method': 'POST',
+			'params': {'uploadType': 'multipart'},
+			'headers': { 'Content-Type': 'multipart/form-data; boundary="' + boundary + '"', 'Authorization': 'Bearer ' + auth_token, },
+			'body': multipartRequestBody 
+		}).execute(function(file) { 
+			console.log("Wrote to file " + file.name + " id: " + file.id); 
+		}, function(error){
+				console.log(error);
+		});  
+	}
+	
+	
+	//Function meant to handle adding a new post to the Wall JSON in Google Drive
+	//Essentially in four steps:
+	//1. Find and download current Wall JSON
+	//2. Append new post to Wall JSON
+	//3. Delete current Wall JSON
+	//4. Post updated Wall JSON
+	function AddPostToWall()
+	{
+		var fileName = "name= " + "'myWallJSON.txt'";
+		var isTrashed = "trashed = false"
+		var queryList = fileName + 'and' + isTrashed;
+		var mainDir = '1Fu27Zw3PYHE5CfjYbuzV-KqB56Pzfdsc'
+		var fileID;
+		
+		//Step 1: Download JSON file
+		
+		//Find Wall JSON
+		gapi.client.drive.files.list(
+		{    
+			 'q' : queryList
+		}).then(function(response) 
+		{
+			console.log(response.result);
+			fileID = response.result.files[0].id;
+			
+			//Get contents of Wall JSON
+			gapi.client.drive.files.get({
+				'fileId' : fileID,
+				alt : 'media'
+			}).then(function(response)
+			{
+			
+								
+				//Step 2: Create new one with new post in it
+				//Response.body has is current Wall JSON content
+				//DO SOME APPENDING STUFF
+				//DO SOME APPENDING STUFF
+				//DO SOME APPENDING STUFF
+				
+				
+				//Step 3: Delete old Wall JSON	
+				gapi.client.drive.files.delete({
+					'fileId' : fileID
+				}).then(function(response)
+				{
+					console.log(response);
+				}), function(reason)
+				{
+					console.log(reason);
+				}
+				
+				//Step 4: Post updated Wall JSON
+				console.log(response.body);
+				
+				//Pass this function the updated Wall JSON with new post appended 
+				//instead of response.body.
+				
+				//MainDir is id of POSN main directory, currently hard coded
+				updateWallJSON(mainDir, response.body);
+				
+			}, function(reason)
+			{
+				console.log('Error: ' + reason.result.error.message);
+			});
+		}, function(reason) 
+		{
+			console.log('Error: ' + reason.result.error.message);
+		});
+	}
+	  //gives a list of web links for images in the Photos folder
+	  function getPhotoLinks()
+	  {
+		var order = 'folder'
+		
+		//Line below is hard coded
+		var photoDirectory = "'1VsFK73KutpZziLFYIihvXlhPIHpH8zbd' in parents"
+		
+		
+		var isTrashed = "trashed = false"
+		//Function returns a list of files in google drive
+        var listPromise = Promise.resolve(gapi.client.drive.files.list(
+			{    
+				 'orderBy': order,
+				 'q' : photoDirectory
+			}).then(function(response) 
+			{
+				console.log(response.result.files);
+				var filesList = response.result.files;
+				var webLinkList = [];
+				getWebLinks( filesList, webLinkList );
+				return webLinkList;
+			}, function(reason) 
+			{
+				console.log('Error: ' + reason.result.error.message);
+				apiBackoff(getPhotoLinks());
+			}));
+			
+		listPromise.then(function(value)
+		{
+			//Do whatever you need with list of Weblinks
+			//value is webLinkList
+			console.log(value);
+			return value;
+		}).then(function(value)
+		{
+			//Do whatever you need with list of Weblinks
+			//value is webLinkList
+		});
+	  }
+	 
+	  	//Placeholder for the one second delay that will occur when too many API calls
+		//are made in a second. (defined as 10 per sec by Google)		
+		//Not used
+		async function apiBackoff(apiFunction, apiVar1, apiVar2) 
+		{
+			var delay = 1000;
+			console.log('timeout');
+			if(filesList)
+			{
+				console.log('Requests from batch failed');
+				setTimeout(function() 
+				{  
+					apiFunction(apiVar1, apiVar2);
+				}, delay);
+			}
+			else
+			{
+				setTimeout(function() 
+				{  
+					console.log('Not weblink, regular call');
+					apiFunction();
+				}, delay);
+			}
+		}
+		
 	//Successfully implemented batching of requests to allow for 10+
-	//Needs some error checking
+	//gets a list of web content links
 	function getWebLinks( filesList, webLinkList )
 	{
 		//Gets web content links for all the photos in the folder
@@ -546,6 +715,8 @@ function getPhotoLinks()
 		var callList = [];
 		var httpBatch = gapi.client.newBatch()
 		var webLink;
+		
+		//Adds all requests for webContentLinks in a batch
 		while( i < filesList.length )
 		{
 			currentID = filesList[i].id;
@@ -556,26 +727,35 @@ function getPhotoLinks()
 			}));
 			httpBatch.add(callList[i])
 			i++;
-		}
-		console.log(httpBatch);
+		}		
+		//Sends batch to Google
 		httpBatch.then(function(onFulfilled)
 		{
 			i = 0;
 			myResult = onFulfilled.result;
-			while( i < filesList.length )
+			parseRes = JSON.stringify(onFulfilled.result);
+			fail = parseRes.search('403');
+			if(fail == -1)
 			{
-				callList[i].then(function(response)
+				while( i < filesList.length )
 				{
-					webLink = JSON.parse(response.body);
-					webLinkList.push(webLink.webContentLink);
-				}, function(reason)
-				{
-					console.log('Error: ' + reason.result.error.message);
-				});
-				i++;
+					callList[i].then(function(response)
+					{
+						webLink = JSON.parse(response.body);
+						webLinkList.push(webLink.webContentLink);
+					}, function(reason)
+					{
+						console.log('Error: ' + reason.result.error.message);
+					});
+					i++;
+				}
+			}
+			else
+			{
+				console.log(filesList);
+				apiBackoff( getWebLinks, filesList, webLinkList)
 			}
 		})
-		console.log(webLinkList)
 	}
 	  	
 		
@@ -595,27 +775,28 @@ function getPhotoLinks()
 			console.log('Two second later');
 		}
 
-	  //Not fully functional
-	  function getPhotosFolderId ()
-	  {
-		  	var folderName = "name= 'Photos'";
-			var isTrashed = "trashed = false"
-			var query = folderName + 'and' + isTrashed;
-			var photosID;
-			gapi.client.drive.files.list(
-			{    
-				 'q' : query
-			}).then(function(response) 
-			{
-				console.log(response.result);
-				photosID = response.result.files[0].id;
-				console.log(photosID)
-			}, function(reason) 
-			{
-				console.log('Error: ' + reason.result.error.message);
-			});
-			return photosID;
-	  }
+	//Works but cannot return
+	function getFileId( fileName )
+	{
+		//Pass like "'Name'" to function
+		var fileName = "name= " + fileName;
+		console.log(fileName);
+		var isTrashed = "trashed = false"
+		var query = fileName + 'and' + isTrashed;
+		var fileID;
+		gapi.client.drive.files.list(
+		{    
+			 'q' : query
+		}).then(function(response) 
+		{
+			console.log(response.result);
+			fileID = response.result.files[0].id;
+			console.log(fileID)
+		}, function(reason) 
+		{
+			console.log('Error: ' + reason.result.error.message);
+		});
+	}
 	  
 	  //Old Version of function before batching was implemented
 	  function getWebLinksOld( filesList, webLinkList, i )
@@ -640,34 +821,5 @@ function getPhotoLinks()
 			}
 			console.log(webLinkList)
 	  }
-	  
-	  
-	  
-		//Placeholder for the one second delay that will occur when too many API calls
-		//are made in a second. (defined as 10 per sec by Google)		
-		//Not used
-		async function webLinkBackoff(filesID, webLinkList) 
-		{
-			console.log(filesID);
-			var delay = 1000;
-			console.log('timeout');
-			setTimeout(function() 
-			{  
-				console.log('Try again');
-				gapi.client.drive.files.get(
-				{
-					'fileId': filesID,
-					fields: 'webContentLink'
-				}).then(function(response) 
-				{
-					webLink = JSON.parse(response.body);
-					webLinkList.push(webLink.webContentLink);
-				},function(reason)
-				{
-					console.log('Error: ' + reason.result.error.message);
-				});
-			}, delay);
-			console.log(webLinkList);
-		}
 		
 	
